d72cf3392c4a7c8b8bdda4b8e15587d7f661bd87
cathy910196 <cathy910196@gmail.com>
2017-06-24 23:02:07 +0800

    cardview測試結合fragment，順便改密碼

--- app/src/main/java/health/binodata/health_app_test/main_tab/tab_tech.java
+++ app/src/main/java/health/binodata/health_app_test/main_tab/tab_tech.java
@@ -4,9 +4,16 @@ package health.binodata.health_app_test.main_tab;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import health.binodata.health_app_test.R;
 
@@ -19,7 +26,51 @@ public class tab_tech extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.tab_tech,container,false);
-
+        ArrayList<String> myDataset = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            myDataset.add(i + "");
+        }
+        MyAdapter myAdapter = new MyAdapter(myDataset);
+        RecyclerView mList = (RecyclerView) v.findViewById(R.id.list_view);
+        final FragmentActivity c = getActivity();
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(c);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mList.setLayoutManager(layoutManager);
+        mList.setAdapter(myAdapter);
         return v;
+    }
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private List<String> mData;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.info_text);
+            }
+        }
+
+        public MyAdapter(List<String> data) {
+            mData = data;
+        }
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.mTextView.setText(mData.get(position));
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
     }
 }
